3753536a84ac0a8866caad8136164f9b0430127a
Platform <jdejager589@gmail.com>
2026-05-24 17:50:30 +0200

    feat(mail): add mail DTOs and response interfaces

---
+++ api/src/mail/mail.dto.ts
@@ -0,0 +1,63 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsBoolean } from 'class-validator';
+
+export class SendMailDto {
+  @IsEmail()
+  to!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  subject!: string;
+
+  @IsString()
+  bodyHtml!: string;
+}
+
+export class VacationDto {
+  @IsBoolean()
+  active!: boolean;
+
+  @IsOptional()
+  @IsString()
+  subject?: string;
+
+  @IsOptional()
+  @IsString()
+  body?: string;
+}
+
+export interface FolderStat {
+  name: string;
+  unread: number;
+}
+
+export interface MailStats {
+  unread: number;
+  folders: FolderStat[];
+}
+
+export interface MailMessage {
+  uid: number;
+  from: string;
+  subject: string;
+  date: string;
+  seen: boolean;
+  hasAttachment: boolean;
+}
+
+export interface MailDetail {
+  uid: number;
+  from: string;
+  to: string;
+  cc: string;
+  subject: string;
+  date: string;
+  seen: boolean;
+  bodyHtml: string;
+  bodyText: string;
+}
+
+export interface VacationState {
+  active: boolean;
+  subject: string;
+  body: string;
+}
